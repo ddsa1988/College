a25bf12fd1280837f80e5fb104cf50a6fe24a40e
Diego Alexandre <d.alexandre@acxautomation.com>
2024-06-10 20:10:45 -0300

    commit

---
+++ DataStructures/Class004/AVLTree.py
@@ -0,0 +1,2 @@
+﻿class AVL:
+    pass
---
+++ DataStructures/Class004/BinarySearchTree.py
@@ -0,0 +1,16 @@
+﻿class Node:
+    def __init__(self, element=None):
+        self.element = element
+        self.left = None
+        self.right = None
+
+    def __repr__(self):
+        return f"Element: {self.element}\nLeft: {self.left}\nRight: {self.right}"
+
+
+class BST:
+    pass
+
+
+node = Node("Diego")
+print(node)
