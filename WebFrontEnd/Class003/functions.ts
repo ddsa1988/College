6c285416b2958188343c03d66049a3c761d5e6d9
Diego Alexandre <93630291+ddsa1988@users.noreply.github.com>
2025-05-29 11:43:15 -0300

    Delete WebFrontEnd directory

--- WebFrontEnd/Class003/functions.ts
+++
@@ -1,5 +0,0 @@
-function sum(a: number, b: number): number {
-    return a + b;
-}
-
-let result: number = sum(10, 15);
--- WebFrontEnd/Class003/variables.ts
+++
@@ -1,4 +0,0 @@
-let firstName: string = "Diego";
-let age: number = 37;
-let active: boolean = true;
-let birthdate: Date = new Date("1988-01-22");
